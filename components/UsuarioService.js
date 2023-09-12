3e3ce847207a1c7f16186b6a6cb03a5ccbe1bff1
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-12 08:55:11 -0300

    UsuarioService

---
+++ components/UsuarioService.js
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+//npmi@react-native-async-storage/async-storage
+//Definicionesdeconstantes. const
+USERNAME_KEY = "LOGIN_username";
+constPASSWORD_KEY = "LOGIN_password";
+class UsuarioService {
+  static login = async (userName, password) => {
+    //Obtienelascredencialesalmacenadaseintentaloguearse.
+    let isValid = false;
+    returnisValid;
+  };
+
+  static automaticlogin = async () => {
+    //Obtienelascredencialesalmacenadaseintentaloguearse.
+    let isValid = false;
+    returnisValid;
+  };
+  //Eliminalascredencialesalmacenadasalcerrarsesión
+
+  static eliminarCredenciales = async () => {};
+
+  static almacenarCredenciales = async (userName, password) => {
+    //AlmacenalascredencialesenelasyncStorage
+    //(paraleerlasaliniciarlapróximavez)
+  };
+  static obtenerCredenciales = async () => {
+    const returnValue = { userName: storedUserName, password: storedPassword };
+    return returnValue;
+  };
+}
+export default UsuarioService;
